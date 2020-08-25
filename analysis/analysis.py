25e07a37d8345f3dfd74ef9be34942c16c5ace7b
Joel Savitz <joelsavitz@gmail.com>
2020-08-24 20:42:39 -0400

    we have a curve :D

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -4,6 +4,7 @@ import datetime
 import re
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import numpy
 
 def getlines(filename):
     f = open(filename)
@@ -21,33 +22,44 @@ def calculate_duration(start_datetime_str, end_datetime_str):
     end_datetime = datetime.datetime.strptime(end_datetime_str, "%Y-%m-%d %H:%M:%S.%f")
     return end_datetime - start_datetime
 
+# paramaters: t(ime), I(nitial), C(ooling coefficient), O(ffset)
+def exponential_decay(t, I, C, O):
+    return I * numpy.exp(-C * t) + O
+
+# If we don't have datafile, we can't do anything
 if len(sys.argv) < 2:
     print("usage: analysis.py <datafile>")
     sys.exit(0)
 
 
+# Get the raw data from the datafile
 datafile = sys.argv[1]
 lines = getlines(datafile)
 
-
+# Calculate the duration of the sensor reading
 duration = calculate_duration(stamp_stripper(lines[0]), stamp_stripper(lines[-1]))
 
 # Turn temperature readings into differences of type float
-y_data = [nums[1] - nums[0] for nums in [list(map(float, s[0:-1].split(' '))) for s in lines[1:-1]]]
+y_data = [nums[1] - nums[0] for nums in [list(map(numpy.float64, s[0:-1].split(' '))) for s in lines[1:-1]]]
 
-# print(data_list)
-# print(duration)
-
+# Calculate the time interval by dividing the total duration by the number of data points
 x_range = len(y_data)
 x_units = (duration / x_range).total_seconds()
-x_data = [i * x_units for i in range(x_range)]
+x_data = numpy.array([numpy.float64(i * x_units) for i in range(x_range)])
 
-print(x_range, x_units)
-print(x_data)
+# Perform non-linear least squares curve fitting using the scipy library
+popt, pcov = curve_fit(exponential_decay, x_data, y_data)
 
-plt.plot(x_data, y_data, 'g--', label='data')
+# Plot the raw data in blue dashed lines
+plt.plot(x_data, y_data, 'b--', label='data')
 
-plt.xlabel('Time (units = seconds, interval = {})'.format(x_units))
+# Plot the curve we fitted to the data
+plt.plot(x_data, exponential_decay(x_data, *popt), 'r-',
+        label='fit:I=%.3E, C=%.3E, O=%.3E' % tuple(popt))
+
+plt.xlabel('Time (units = seconds, interval = %.3f)')
 plt.ylabel('Temperature (units = degrees celcius)')
+plt.legend()
 
+# The result :)
 plt.show()
